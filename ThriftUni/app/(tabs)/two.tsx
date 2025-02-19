83ca9fa41288c59d09b4b850eaaf3613b1b44549
Jvstinoo <justin.balbuena@upr.edu>
2025-02-19 00:06:02 -0400

    Individual listing page. Still incomplete.

--- ThriftUni/app/(tabs)/two.tsx
+++ ThriftUni/app/(tabs)/two.tsx
@@ -2,15 +2,28 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { ListingDetails } from '@/components/DisplayIndividualListing';
+import DisplayIndividualListing from '@/components/DisplayIndividualListing';
 
 export default function TabTwoScreen() {
+  const exampleListing: ListingDetails = {
+      title: "Agua",
+      pictures: [],
+      description: "Guitarra para venta.",
+      sellerName: "Juan",
+      sellerBio: "Soy Juan",
+    };
+    
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
-    </View>
-  );
+    DisplayIndividualListing(exampleListing)
+  )
+//  return (
+//    <View style={styles.container}>
+//      <Text style={styles.title}>Tab Two</Text>
+//      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+//      <EditScreenInfo path="app/(tabs)/two.tsx" />
+//    </View>
+//  );
 }
 
 const styles = StyleSheet.create({
