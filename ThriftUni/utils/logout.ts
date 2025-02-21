4e190e1531de5ca71d4fc33a351cd8839c516cc2
J3SSY-ANDU <jessy.andujar@upr.edu>
2025-02-21 13:44:23 -0400

    deleted firebaseConfig.js and made changes to logout file to update the firebase.config file import path

--- ThriftUni/utils/logout.ts
+++ ThriftUni/utils/logout.ts
@@ -1,5 +1,5 @@
 import { signOut } from 'firebase/auth';
-import { auth } from '../firebaseConfig';
+import { auth } from '../firebase/firebase.config';
 
 /**
  * Logs out the currently authenticated user using Firebase Authentication.
