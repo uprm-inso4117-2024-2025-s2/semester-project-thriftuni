14814956667b5c930a0f9163e693977587fdcac7
Janiel777 <janjannunez777@gmail.com>
2025-03-20 13:57:49 -0400

    Tests were changed so they do not fail intentionally

--- ThriftUni/components/__tests__/ProductCard-test.ts
+++ ThriftUni/components/__tests__/ProductCard-test.ts
@@ -4,7 +4,7 @@ import {describe, expect, it} from "@jest/globals";
 // Test formatPrice function
 describe('formatPrice helper function', () => {
   it('should return formatted price', () => {
-    expect(formatCurrency(1000)).toBe('$999,9999.99'); //expected ($1,000.00)
+    expect(formatCurrency(1000)).toBe('$1,000.00');
   }),
   it('should return formatted price', () => {
     expect(formatCurrency(0)).toBe('$0.00');
