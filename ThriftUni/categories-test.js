ac26a63bacc9a49215eee366731b735a88dc9091
Jorge Rivera <112115387+JorgeRivera94@users.noreply.github.com>
2025-03-20 21:30:03 -0400

    Corrected trests and structure.

--- ThriftUni/categories-test.js
+++ ThriftUni/categories-test.js
@@ -1,6 +1,7 @@
 // imports
 const { initializeTestEnvironment, assertSucceeds, assertFails } = require("@firebase/rules-unit-testing");
 const admin = require("firebase-admin");
+const assert = require("assert");
 
 // Initialize admin for rw permissions
 const PROJECT_ID = "thriftuni-b345a";
@@ -12,18 +13,20 @@ admin.initializeApp({
 
 const adminDb = admin.firestore();
 
+// THESE MOCK FUNCTIONS ARE TO BE REPLACED WITH IMPLEMENTED FUNCTIONS FROM FRONTEND AND BACKEND INTERACTION
+
 // mock function to create listing_categories documents
-const createListingCategoriesDoc = async (listingId, categoryId) => {
-    await adminDb.collection("listing_categories").doc(listingId + categoryId).set({
-        listing_id: listingId,
-        category_id: categoryId
+const createListingCategoriesDoc = async (listingsId, categoriesId) => {
+    await adminDb.collection("listing_categories").doc(listingsId + categoriesId).set({
+        listing_id: listingsId,
+        category_id: categoriesId
     });
 };
 
 // mock function to create listing documents
-const createListingDoc = async (listingId, title, description, price, category_id, condition, latitude, longitude) => {
-    await adminDb.collection("listing").doc(listingId).set({
-        listing_id: listingId,
+const createListingsDoc = async (listingsId, title, description, price, category_id, condition, latitude, longitude) => {
+    await adminDb.collection("listings").doc(listingsId).set({
+        listing_id: listingsId,
         title: title,
         description: description,
         price: price,
@@ -38,76 +41,97 @@ const createListingDoc = async (listingId, title, description, price, category_i
 };
 
 // mock function to create categories documents
-const createCategoriesDoc = async (categoryId, name, description) => {
-    await adminDb.collection("categories").doc(categoryId).set({
-        category_id: categoryId,
+const createCategoriesDoc = async (categoriesId, name, description) => {
+    await adminDb.collection("categories").doc(categoriesId).set({
+        category_id: categoriesId,
         name: name,
         description: description,
         created_at: admin.firestore.FieldValue.serverTimestamp()
     });
 };
 
-// Initialize the test environment
-const testEnv = await initializeTestEnvironment({
-    projectId: PROJECT_ID
-});
+(async () => {
+    console.log("Creating documents in Firestore.");
 
-// Create user contexts
-const unauthenticatedContext = testEnv.unauthenticatedContext();
-const userContext = testEnv.authenticatedContext("user");
-
-// Obtain Firestore instances
-const unauthDb = unauthenticatedContext.firestore();
-const userDb = userContext.firestore();
-
-describe("Firesotre rules for categories", () => {
-    beforeAll(async () => {
-        // Create initial documents
+    // Create initial documents in Firestore
+    try {
         await createCategoriesDoc("1", "Book", "Book for sale");
-        await createListingDoc("1", "My Book", "Mock Description", 10, 1, 0, 0, 0);
+        await createListingsDoc("1", "My Book", "Mock Description", 10, 1, ["New"], 0, 0);
         await createListingCategoriesDoc("1", "1");
+
+        console.log("All initial documents have been successfully created.");
+    } catch (error) {
+        console.error("Error creating initial documents in Firestore:", error.message);
+    };
+
+    // Initialize the test environment
+    const testEnv = await initializeTestEnvironment({
+        projectId: PROJECT_ID
     });
 
+    //////////////////////////////////////////////////////////////////
     afterAll(async () => {
-        // Delete all created documents
-        await adminDb.collection("categories").doc("1").delete();
-        await adminDb.collection("listing").doc("1").delete();
-        await adminDb.collection("listing_categories").doc("11").delete();
-    });
+        // Ensure all created documents are deleted
+        const categoriesDoc1 = await adminDb.collection("categories").doc("1").get();
+        if (categoriesDoc1.exists) {
+            await adminDb.collection("categories").doc("1").delete();
+        };
+        const categoriesDoc2 = await adminDb.collection("categories").doc("2").get();
+        if (categoriesDoc2.exists) {
+            await adminDb.collection("categories").doc("2").delete();
+        };
+        const listingsDoc = await adminDb.collection("listings").doc("1").get();
+        if (listingsDoc.exists) {
+            await adminDb.collection("listings").doc("1").delete();
+        };
+        const listingCategoriesDoc = await adminDb.collection("listing_categories").doc("11").get();
+        if (listingCategoriesDoc.exists) {
+            await adminDb.collection("listing_categories").doc("11").delete();
+        };
 
-    // Test 1: Test for duplicate categories docuent by evaluating name field
-    test("Test for duplicate categories document by evaluating name field", async () => {
-        await assertFails(createCategoriesDoc("2", "Book", "Book for sale"));
+        await testEnv.cleanup();
     });
+    //////////////////////////////////////////////////////////////////
+
+    console.log("Running tests.");
+
+    //Test 1: Test for duplicate categories docuent by evaluating name field
+    try {
+        await createCategoriesDoc("2", "Book", "Book for sale");
+        console.log("Test for duplicate categories document by evaluating name field: PASSED");
+    } catch (error) {
+        console.log(`FAILED: Test for duplicate categories document by evaluating name field\nError: ${error.message}\n`);
+    };
 
     // Test 2: Test for duplicate listing_categories document by evaluating category_id and listing_id fields
-    test("Test for duplicate listing_categories document by evaluating category_id and listing_id fields", async () => {
-        await assertFails(createListingCategoriesDoc("1", "1"));
-    });
+    try {
+        await createListingCategoriesDoc("1", "1");
+        console.log("Test for duplicate listing_categories document by evaluating category_id and listing_id fields: PASSED");
+    } catch (error) {
+        console.log(`FAILED: Test for duplicate listing_categories document by evaluating category_id and listing_id fields\n Error: ${error.message}\n`);
+    };
 
-    // test 3: Test for category_id being set to null when category document is deleted
-    test("Test for category_id field of listing_category document being set to null when category document is deleted", async () => {
+    // Test 3: Test for category_id being set to null when category document is deleted
+    try {
         await adminDb.collection("categories").doc("1").delete();
         const listingCategoriesDoc = await adminDb.collection("listing_categories").doc("11").get();
-        expect(listingCategoriesDoc.data().category_id).toBeNull();
-    });
+        assert.strictEqual(listingCategoriesDoc.data().category_id, null)
+        console.log("Test for category_id field of listing_category document being set to null when category document is deleted: PASSED");
+    } catch (error) {
+        console.log(`FAILED: Test for category_id field of listing_category document being set to null when category document is deleted\nError: ${error.message}\n`);
+    };
 
     // Test 4: Test for listing_categories document being deleted when listing document is deleted
-    test("Test for listing_categories document being deleted when listing document is deleted", async () => {
-        await adminDb.collection("listing").doc("1").delete();
-        const listingCategoriesDoc = await adminDb.collection("listing_categories").doc("11").get();
-        expect(listingCategoriesDoc.exists).toBeFalsy();
-    });
-});
+    try {
+        await adminDb.collection("listings").doc("1").delete();
+        const listingCategoriesDoc2 = await adminDb.collection("listing_categories").doc("11").get();
+        assert.strictEqual(listingCategoriesDoc2.exists, false);
+        console.log("Test for listing_categories document being deleted when listing document is deleted: PASSED");
+    } catch (error) {
+        console.log(`FAILED: Test for listing_categories document being deleted when listing document is deleted\nError: ${error.message}\n`);
+    };
 
-// test general stucture:
-// 1) create a categories document
-// 2) create a listing document
-// 3) create a listing_categories document
-// 4) read the listing_categories document and check if the category_id and listing_id are correct
-// 5) delete the listing document
-// 6) read the listing_categories document, it should not exist anymore
-// 7) delete the categories document
+    console.log("🔥 All tests have been executed.");
 
-
-
+    await testEnv.cleanup();
+})();
